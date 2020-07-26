df6a91c8041842da341a44e200d9a7f706d66364
trnnn <cheongbong007@gmail.com>
2020-07-26 15:33:34 +0900

    add calculateSerializingSize method for byte[]

--- src/easysocket/serialize/Serializable.java
+++ src/easysocket/serialize/Serializable.java
@@ -9,100 +9,107 @@ import easysocket.packet.ByteBufferManager;
 public interface Serializable {
 
 
-    void serialize(ByteBufferManager buffer);
+	void serialize(ByteBufferManager buffer);
 
-    int getSerializingSize();
+	int getSerializingSize();
 
-    static final Charset defaultCharset = StandardCharsets.UTF_8;
+	static final Charset defaultCharset = StandardCharsets.UTF_8;
 
-    public static int calculateSerializingSize(String str) {
-        if (str == null || str.equals("")) {
-            return 4;
-        }
-        int size = str.getBytes(defaultCharset).length;
-        return size + 4;
-    }
+	public static int calculateSerializingSize(String str) {
+		if (str == null || str.equals("")) {
+			return 4;
+		}
+		int size = str.getBytes(defaultCharset).length;
+		return size + 4;
+	}
 
-    public static <T extends Serializable> int calculateSerializingSize(Collection<T> list) {
-        int size = 4;
-        if (list != null) {
-            for (T t : list) {
-                size += (1 + t.getSerializingSize());
-            }
-        }
-        return size;
-    }
+	public static <T extends Serializable> int calculateSerializingSize(Collection<T> list) {
+		int size = 4;
+		if (list != null) {
+			for (T t : list) {
+				size += (1 + t.getSerializingSize());
+			}
+		}
+		return size;
+	}
 
-    public static <T extends Serializable> int calculateSerializingSize(T t) {
-        return 1 + (t == null ? 0 : t.getSerializingSize());
-    }
+	public static <T extends Serializable> int calculateSerializingSize(T t) {
+		return 1 + (t == null ? 0 : t.getSerializingSize());
+	}
 
-    public static int calculateSerializingSize(long[] array) {
-        int size = 4;
-        if (array != null) {
-            size += 8 * array.length;
-        }
-        return size;
-    }
+	public static int calculateSerializingSize(long[] array) {
+		int size = 4;
+		if (array != null) {
+			size += 8 * array.length;
+		}
+		return size;
+	}
 
-    public static int calculateSerializingSize(int[] array) {
-        int size = 4;
-        if (array != null) {
-            size += 4 * array.length;
-        }
-        return size;
-    }
+	public static int calculateSerializingSize(int[] array) {
+		int size = 4;
+		if (array != null) {
+			size += 4 * array.length;
+		}
+		return size;
+	}
 
-    public static int calculateSerializingSizeIntList(Collection<Integer> array) {
-        int size = 4;
-        if (array != null) {
-            size += 4 * array.size();
-        }
-        return size;
-    }
+	public static int calculateSerializingSizeIntList(Collection<Integer> array) {
+		int size = 4;
+		if (array != null) {
+			size += 4 * array.size();
+		}
+		return size;
+	}
 
-    public static int calculateSerializingSizeLongList(Collection<Long> array) {
-        int size = 4;
-        if (array != null) {
-            size += 8 * array.size();
-        }
-        return size;
-    }
+	public static int calculateSerializingSizeLongList(Collection<Long> array) {
+		int size = 4;
+		if (array != null) {
+			size += 8 * array.size();
+		}
+		return size;
+	}
 
-    public static int calculateSerializingSize(float[] array) {
-        int size = 4;
-        if (array != null) {
-            size += 4 * array.length;
-        }
-        return size;
-    }
+	public static int calculateSerializingSize(float[] array) {
+		int size = 4;
+		if (array != null) {
+			size += 4 * array.length;
+		}
+		return size;
+	}
 
-    public static int calculateSerializingSize(boolean[] array) {
-        int size = 4;
-        if (array != null) {
-            size += array.length;
-        }
-        return size;
-    }
+	public static int calculateSerializingSize(boolean[] array) {
+		int size = 4;
+		if (array != null) {
+			size += array.length;
+		}
+		return size;
+	}
 
-    public static int calculateSerializingSize(String[] array) {
-        int size = 4;
-        if (array != null) {
-            for (String str : array) {
-                size += calculateSerializingSize(str);
-            }
-        }
-        return size;
-    }
+	public static int calculateSerializingSize(String[] array) {
+		int size = 4;
+		if (array != null) {
+			for (String str : array) {
+				size += calculateSerializingSize(str);
+			}
+		}
+		return size;
+	}
 
-    public static <T extends Serializable> int calculateSerializingSize(T[] array) {
-        int size = 4;
-        if (array != null) {
-            for (T t : array) {
-                size += calculateSerializingSize(t);
-            }
-        }
-        return size;
-    }
+	public static <T extends Serializable> int calculateSerializingSize(T[] array) {
+		int size = 4;
+		if (array != null) {
+			for (T t : array) {
+				size += calculateSerializingSize(t);
+			}
+		}
+		return size;
+	}
 
+	public static int calculateSerializingSize(byte[] array) {
+		int size = 4;
+		if (array != null) {
+			size += array.length;
+		}
+		return size;
+	}
 }
